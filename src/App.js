e76e3d43946af8335521949224c6f6648c867f6c
7p3m1k <kor0885@gmail.com>
2023-07-30 04:30:02 +0900

    Add: 초기셋팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./Components/Header";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<SignIn />}></Route>
+        <Route path="/SignUp" element={<SignUp />}></Route>
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
