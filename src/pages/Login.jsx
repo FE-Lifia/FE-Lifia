d355b31f8ee98c3d0f642b107f08fc16e3c33a4b
seyoung <aras3640@gmail.com>
2023-09-02 21:51:02 +0900

    [feat] Login 화면

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,75 +1,97 @@
 import React from "react";
 import styled from "styled-components";
-import LoginButton from "../components/Button/LoginButton"
-import LogininputBox from "../components/Input Box/LoginInputBox"
-//로그인 페이지
+import LoginButton from "../components/Button/LoginButton";
+import InputBox from "../components/Input Box/InputBox";
+// 로그인 페이지
 
-const Login = ({ onChangeId, onChangePassword, onClickLoginButton, onClickFindAccount, onClickSignUp }) => {
+    const Login = ({
+    onChangeId,
+    onChangePassword,
+    onClickLoginButton, //로그인
+    onClickFindAccount, //비밀번호 찾기
+    onClickSignUp,
+    }) => {
     return (
         <>
-            <LoginVeiwLayout style={{ display: "flex", flexDirection: "column", backgroundColor: "#5CA1E4" }}>
-            <LogoImage src={"../img/mainlogo.png"}  />
-                <LoginContainer style={{ display: "flex", flexDirection: "column", gap: "30px", marginBottom: "10px" }}>
-                    <InputBoxContainer style={{ display: "flex", flexDirection: "column", gap: "10px", marginBottom: "10px" }}>
-                        <LogininputBox placeholder="ID" onChange={onChangeId} />
-                        <LogininputBox type="password" placeholder="PASSWORD" onChange={onChangePassword} />
-                    </InputBoxContainer>
-                    <LoginButton onClick={onClickLoginButton} title="LOGIN"></LoginButton>
+            <LoginViewLayout style={{ display: "flex", flexDirection: "column" }}>
+                <LogoImage src={"../img/mainlogo.png"} />
+                <LoginContainer>
+                <InputBoxContainer style={{ display: "flex", flexDirection: "column", gap: "10px", marginBottom: "30px" }}>
+                    <InputBox placeholder="ID" onChange={onChangeId} />
+                    <InputBox type="password" placeholder="PASSWORD" onChange={onChangePassword} />
+                </InputBoxContainer>
+                <LoginButton onClick={onClickLoginButton} title="LOGIN"></LoginButton>
                 </LoginContainer>
-            </LoginVeiwLayout>
-        </>
+                <SignUpButton onClick={onClickSignUp}>
+                아직 아이디가 없다면? &nbsp;회원가입
+                </SignUpButton>
+            </LoginViewLayout>
+            </>
     );
 };
 
-const LoginVeiwLayout = styled.div`
-    display: flex;
-    width: 100%;
-    height: 90vh;
-    flex-direction: row;
+const LoginViewLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  background: linear-gradient(to right, #44BAD8, #5CA1E4, #6F96E1);
+  flex-direction: column;
 
-    @media (max-width: 760px) {
-        flex-direction: column;
-    }
+  @media (max-width: 760px) {
+    flex-direction: column;
+  }
 `;
 
 const LoginContainer = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 50px;
-    margin: left;
-    border-radius: 20px;
-    background: linear-gradient(180deg, #93C5EA 50%, #99BEEB 0%);
+  width: 35%; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2); /* 그림자 효과 추가 */
+  background: linear-gradient(to right, #8FC7EA, #94C4E9, #99C1E8, #9CBEE8, #9FBCEA);
 
-    @media (max-width: 760px) {
-        width: 100%;
-        margin: 0;
-    }
+  @media (max-width: 760px) {
+    width: 100%;
+    margin: 0;
+  }
 `;
 
 const InputBoxContainer = styled.div`
-    margin-top: 100px;
-    width: 100%;
-    font-family: nexon-regular;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 18px;
-    color: #b6b6b6;
+  margin-top: 20px;
+  width: 100%;
+  font-style: normal;
+  font-size: 18px;
+  color: #b6b6b6;
+  border-radius: 10px;
 
-    @media (max-width: 760px) {
-        margin-top: 50px;
-    }
+  @media (max-width: 760px) {
+    margin-top: 10px;
+  }
 `;
 
 const LogoImage = styled.img`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 40%;
-    margin: 0 auto;
+  width: 15%;
+  margin: 0 auto;
+  margin-bottom: 10px;
 `;
 
+const SignUpButton = styled.button`
+  height: 50px;
+  margin-top: 10px;
+  font-size: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  margin: 0 auto;
+
+  &:hover {
+    color: #4169e1;
+  }
+`;
 
 export default Login;
