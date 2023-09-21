98211b40ab01f348495067e315479da90a68aafe
J with YOU <khy960101@gmail.com>
2023-09-21 13:13:26 +0900

    Footer 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./components/pages/Home";
 import SignIn from "./components/pages/SignIn";
 import SignUp from "./components/pages/SignUp";
@@ -17,6 +18,7 @@ function App() {
         <Route path="/SignUp" element={<SignUp />}></Route>
         <Route path="/*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
